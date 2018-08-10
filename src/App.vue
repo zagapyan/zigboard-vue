915d396b1cb137f0c4e13b4fe817969498e93d17
Zigmund Sun Oo <zigmundsunoo@gmail.com>
2018-08-09 21:32:28 -0500

    [master] init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <ZigBoard v-bind:title="title" v-bind:sounds="sounds"/>
+  </div>
+</template>
+
+<script>
+import ZigBoard from './components/ZigBoard.vue'
+import soundData from './assets/sound-data.json'
+export default {
+  name: 'app',
+  components: {
+    ZigBoard
+  },
+  data: ()=>({
+    title: 'ZigBoard',
+    sounds: soundData
+  })
+}
+</script>
+
+<style>
+html{
+  padding: 0;
+  margin: 0;
+  background: linear-gradient(45deg, #d26ac2, #46c9e5);
+}
+body{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: #000;
+  margin: 5px;
+  padding: 20px;
+  border-radius: 4px;
+}
+</style>
---
+++ src/components/ZigBoard.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="ZigBoard">
+    <h1 ref="title">{{title}}<img :src="logo"/></h1>
+    <ul class="sound-bytes">
+      <li v-for="(sound, index) in sounds" :key="index">
+        <a v-on:click="playSounds(sound.file)">{{ sound.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ZigBoard",
+  props: ["title", "sounds"],
+  data: () => ({
+    logo: require("../assets/logo.png"),
+    rotateActive: false
+  }),
+  methods: {
+    playSounds: fileName => {
+      const soundFile = require(`../assets/audio/${fileName}.mp3`);
+      if (soundFile) {
+        const audio = new Audio(soundFile);
+        audio.play();
+      }
+    },
+    rotate: ()=>{
+      
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.ZigBoard{
+  max-width: 980px;
+  margin: 0 auto;
+}
+h1{
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0;
+  color: #fff;
+  font-size: 4rem;
+  /* background: linear-gradient(270deg, #d26ac2, #46c9e5);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent; */
+}
+h1 img{
+  max-height: 80px;
+  margin-left: 40px;
+  width: auto;
+  transition: .5s transform ease-in-out;
+}
+h1 img:hover{
+  transform: rotate(360deg);
+}
+ul.sound-bytes {
+  list-style-type: none;
+  padding: 0;
+  display: grid;
+  grid-gap: 20px 20px;
+  grid-template-columns: repeat(2, 1fr);
+}
+li {
+  display: inline-block;
+  box-sizing: border-box;
+  border: 1px solid #000;
+  width: 100%;
+  border-radius: 2px;
+  -webkit-text-decoration: none;
+  text-decoration: none;
+  background: linear-gradient(45deg, #d26ac2, #46c9e5);
+  color: #1d2029;
+  font-weight: 800;
+  text-align: center;
+}
+a {
+  box-sizing: border-box;
+  color: #000;
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+  display: inline-flex;
+  text-align: center;
+  padding: 20px;
+  justify-content: center;
+}
+</style>
